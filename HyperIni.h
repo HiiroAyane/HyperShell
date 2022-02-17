4a948f7f6540bbf0211a9d2476fee75a1fd31b05
HiiroAyane <psyr2y@outlook.com>
2022-02-17 16:22:38 +0800

    add buttons

--- HyperIni.h
+++ HyperIni.h
@@ -2,13 +2,14 @@
 #include <QFile>
 #include <QRegularExpression>
 
+#pragma region WidgetText
 inline QString readWidgetTextIni() {
 	QFile file("Text.ini");
 	QString strWidgetText;
 
-	if (!file.exists() || !file.open(QIODevice::ReadOnly)) {
-		strWidgetText = "createSshButton=建立\n连接;\n";
-
+	if (!file.open(QIODevice::ReadOnly)) {
+		strWidgetText = QString::fromUtf8("createSshButton=建立\n连接;\n");
+		strWidgetText += QString::fromUtf8("manageSshButton=管理\n连接;\n");
 		return strWidgetText;
 	}
 
@@ -20,12 +21,21 @@ inline QString readWidgetTextIni() {
 }
 
 inline QString getWidgetText(const QString& keyWord, const QString& text) {
-	QString strRegExp = "(?<=(" + keyWord + "=)).*(?=;)";
-	QRegularExpression regExp(strRegExp);
-	QRegularExpressionMatch match = regExp.match(text);
-	if (match.hasMatch()) {
-		QString strCaptured = match.captured(0);
-		return strCaptured;
-	}
-	else return QString();
+	// Bug
+	//QString strRegExp = "(?<=(" + keyWord + "=)).*(?=;)";
+	//QRegularExpression regExp(strRegExp);
+	//QRegularExpressionMatch match = regExp.match(text);
+	//if (match.hasMatch()) {
+	//	QString strCaptured = match.captured(0);
+	//	return strCaptured;
+	//}
+	//else return QString();
+	int posStart = text.indexOf(keyWord);
+	if (posStart < 0) return QString();
+	posStart += keyWord.size() + 1;
+	int posEnd = text.indexOf(";", posStart);
+	if (posEnd < posStart) return QString();
+	return text.mid(posStart, posEnd - posStart);
 }
+
+#pragma endregion WidgetText
--- HyperShell.cpp
+++ HyperShell.cpp
@@ -1,4 +1,5 @@
 #include "HyperShell.h"
+#include <QScrollArea>
 #include <QPalette>
 #include <QIcon>
 
@@ -19,6 +20,9 @@ void HyperShell::initWidgets()
     this->setWindowFlags(this->windowFlags() & ~Qt::WindowMaximizeButtonHint);
     this->show();
 
+    QScrollArea* fastConnectArea = new QScrollArea(this);
+    fastConnectArea->setGeometry(10, 10, 580, 250);
+
     CustomButton* createSshBtn = new CustomButton(this);
     createSshBtn->setText(getWidgetText("createSshButton", strWidgetText));
     createSshBtn->setNormalBackground(QColor(14, 114, 237));
@@ -27,7 +31,37 @@ void HyperShell::initWidgets()
     createSshBtn->setFontSize("20px");
     createSshBtn->setBorderRadius("20px");
     createSshBtn->setCustomStyle();
-    createSshBtn->setGeometry(100, 200, 100, 100);
+    createSshBtn->setGeometry(20, 280, 100, 100);
+
+    CustomButton* manageSshBtn = new CustomButton(this);
+    manageSshBtn->setText(getWidgetText("manageSshButton", strWidgetText));
+    manageSshBtn->setNormalBackground(QColor(14, 114, 237));
+    manageSshBtn->setHoverBackground(QColor(13, 104, 216));
+    manageSshBtn->setForeground(Qt::white);
+    manageSshBtn->setFontSize("20px");
+    manageSshBtn->setBorderRadius("20px");
+    manageSshBtn->setCustomStyle();
+    manageSshBtn->setGeometry(140, 280, 100, 100);
+
+    CustomButton* optionBtn = new CustomButton(this);
+    optionBtn->setText(getWidgetText("optionButton", strWidgetText));
+    optionBtn->setNormalBackground(QColor(14, 114, 237));
+    optionBtn->setHoverBackground(QColor(13, 104, 216));
+    optionBtn->setForeground(Qt::white);
+    optionBtn->setFontSize("20px");
+    optionBtn->setBorderRadius("20px");
+    optionBtn->setCustomStyle();
+    optionBtn->setGeometry(360, 280, 100, 100);
+
+    CustomButton* settingBtn = new CustomButton(this);
+    settingBtn->setText(getWidgetText("settingButton", strWidgetText));
+    settingBtn->setNormalBackground(QColor(14, 114, 237));
+    settingBtn->setHoverBackground(QColor(13, 104, 216));
+    settingBtn->setForeground(Qt::white);
+    settingBtn->setFontSize("20px");
+    settingBtn->setBorderRadius("20px");
+    settingBtn->setCustomStyle();
+    settingBtn->setGeometry(480, 280, 100, 100);
 
     return;
 }
